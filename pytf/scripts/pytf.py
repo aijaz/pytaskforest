a3e0bafca55c7640bb1be088fae16481e4c35917
Aijaz Ansari <aijaz@users.noreply.github.com>
2024-06-15 23:11:10 -0600

    Got rid of commented code

--- pytf/scripts/pytf.py
+++ pytf/scripts/pytf.py
@@ -8,13 +8,6 @@ import click
 @click.option('--instructions_dir', help='Instructions Directory',
               type=click.Path(file_okay=False, dir_okay=True, exists=True))
 @click.option('--run_wrapper', help='Run Wrapper', type=click.Path(file_okay=True, dir_okay=False, exists=True))
-# @click.option('--wait_time', help='Wait Time', type=click.INT)
-# @click.option('--end_time_hr', help='End Time HH', type=click.IntRange(min=0, max=23))
-# @click.option('--end_time_min', help='End Time MIN', type=click.IntRange(min=0, max=59))
-# @click.option('--collapsed', help='Collapsed', type=click.BOOL)
-# @click.option('--chained', help='Chained', type=click.BOOL)
-# @click.option('--log_level', help='Log Level', type=click.INT)  # TODO: Convert from string to type
-# @click.option('--ignore_regex', help='Ignore Regex', multiple=True)
 @click.option('--config_file', help="Config File", type=click.File('r'))
 @click.pass_context
 def pytf(context,
