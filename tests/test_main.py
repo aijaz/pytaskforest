216646028bbfb50192f939eebcac7691a1b7f7f5
Aijaz Ansari <aijaz@users.noreply.github.com>
2024-06-17 15:59:30 -0500

    Copy family files working

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,12 +11,6 @@ from pytf.pytf.config import Config
 @pytest.fixture
 def no_odd_config():
     return Config.from_str("""
-    log_dir = "l"
-    job_dir = "j"
-    family_dir = "f"
-    instructions_dir = "i"
-    run_wrapper = "r"
-    
     calendars.daily = [
       "*/*/*"
     ]
@@ -24,17 +18,27 @@ def no_odd_config():
       "every Monday */*"
     ]
     ignore_regexes = [
-      ".*[24680]$"
+      ".*[13579]$"
     ]
     """)
 
 def test_make_family_dir_if_necessary(tmp_path, no_odd_config):
     MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, pytz.timezone('America/Denver'))
     todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}-{MM}-{DD}"), MockDateTime.now())
-    no_odd_config.family_dir = todays_family_dir
+    no_odd_config.family_dir = tmp_path
     assert(todays_family_dir == os.path.join(tmp_path, "2024-06-03"))
     assert not os.path.exists(todays_family_dir)
 
+    # make family files
+    for f in ('fa', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'fb'):
+        with open(os.path.join(tmp_path, f), "w") as fp:
+            fp.write('queue: "foo"')
+
+    files = os.listdir(tmp_path)
+    assert(len(files) == 9)
+
     make_family_dir_if_necessary(no_odd_config, todays_family_dir)
     assert os.path.exists(todays_family_dir)
+    files = os.listdir(todays_family_dir)
+    assert(len(files) == 9)
 
