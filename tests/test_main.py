349c8fbc3d1f565cd7a438cde5cdcca43988387a
Aijaz Ansari <aijaz@aijaz.net>
2024-06-21 23:52:43 -0500

    Refactored test_main

--- tests/test_main.py
+++ tests/test_main.py
@@ -23,30 +23,58 @@ def no_odd_config():
     """)
 
 
-def test_make_family_dir_if_necessary(tmp_path, no_odd_config):
+def test_todays_family_dir(tmp_path, no_odd_config):
     MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, 'America/Denver')
     todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}{MM}{DD}"), MockDateTime.now())
     no_odd_config.family_dir = tmp_path
-    assert(todays_family_dir == os.path.join(tmp_path, "20240603"))
+    assert (todays_family_dir == os.path.join(tmp_path, "20240603"))
+
+
+def test_dated_dir_does_not_create_dir(tmp_path, no_odd_config):
+    MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, 'America/Denver')
+    todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}{MM}{DD}"), MockDateTime.now())
+    no_odd_config.family_dir = tmp_path
     assert not os.path.exists(todays_family_dir)
 
+
+def test_make_family_dir_if_necessary_actually_makes_the_directory(tmp_path, no_odd_config):
+    MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, 'America/Denver')
+    todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}{MM}{DD}"), MockDateTime.now())
+    no_odd_config.family_dir = tmp_path
+
+    make_family_files(tmp_path)
+
+    make_family_dir_if_necessary(no_odd_config, todays_family_dir)
+    assert os.path.exists(todays_family_dir)
+
+
+def test_make_family_dir_if_necessary_copies_all_the_files(tmp_path, no_odd_config):
+    MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, 'America/Denver')
+    todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}{MM}{DD}"), MockDateTime.now())
+    no_odd_config.family_dir = tmp_path
+
+    make_family_files(tmp_path)
+
+    make_family_dir_if_necessary(no_odd_config, todays_family_dir)
+    files = os.listdir(todays_family_dir)
+    assert (len(files) == 9)
+
+
+def test_get_families_from_dir_honors_regex(tmp_path, no_odd_config):
+    MockDateTime.set_mock(2024, 6, 3, 1, 2, 3, 'America/Denver')
+    todays_family_dir = dated_dir(os.path.join(tmp_path, "{YYYY}{MM}{DD}"), MockDateTime.now())
+    no_odd_config.family_dir = tmp_path
+
+    make_family_files(tmp_path)
+
+    make_family_dir_if_necessary(no_odd_config, todays_family_dir)
+
+    families: [Family] = get_families_from_dir(family_dir=todays_family_dir, config=no_odd_config)
+    assert ([f.name for f in families] == ['f2', 'f4', 'f6', 'fa', 'fb'])
+
+
+def make_family_files(tmp_path):
     # make family files
     for f in ('fa', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'fb'):
         with open(os.path.join(tmp_path, f), "w") as fp:
             fp.write('queue = "foo"')
-
-    files = os.listdir(tmp_path)
-    assert(len(files) == 9)
-
-    make_family_dir_if_necessary(no_odd_config, todays_family_dir)
-    assert os.path.exists(todays_family_dir)
-    files = os.listdir(todays_family_dir)
-    assert(len(files) == 9)
-
-    families: [Family] = get_families_from_dir(family_dir=todays_family_dir, config=no_odd_config)
-    assert(len(families) == 5)
-    assert(families[0].name == 'f2')
-    assert(families[1].name == 'f4')
-    assert(families[2].name == 'f6')
-    assert(families[3].name == 'fa')
-    assert(families[4].name == 'fb')
