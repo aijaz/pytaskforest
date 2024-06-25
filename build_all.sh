feb488c182c1b1e36261ae0c9724e9249e22a222
Aijaz Ansari <aijaz@aijaz.net>
2024-06-24 19:06:55 -0600

    Updated docker-compose.yml

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Remove all containers
-docker rm -vf $(docker ps -aq) > /dev/null 2>&1
+## Remove all containers
+#docker rm -vf $(docker ps -aq) > /dev/null 2>&1
+#
+## Remove all images
+#docker rmi -f $(docker images -aq) > /dev/null 2>&1
 
-# Remove all images
-docker rmi -f $(docker images -aq) > /dev/null 2>&1
-
-docker network rm aijaz_network
+#docker network rm aijaz_network
 
 docker build -t pytf_i -f Dockerfile_pytf .
+docker build -t worker_i -f Dockerfile_pytf .
